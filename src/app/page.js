8fb09eb2c8e8b237885abd6352845a884a5b2cd4
ShrutiLatthe <lattheshruti@gmail.com>
2024-02-17 15:52:13 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+
+const Home = () => (
+  <>
+    <Header />
+    <Main/>
+    <Footer/>
+  </>
+);
+
+export default Home;
